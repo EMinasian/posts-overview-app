9a846b9f9f0fc945217f092e95930576218d4545
erik.minasian <erik.minasian@coliquio.de>
2024-01-20 16:44:02 +0400

    feat: react router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from "./App";
+import PostDetails from "./components/PostDetails";
 import "./main.css";
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/post-details', element: <PostDetails />}
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="layout">
-      <App />
+      <RouterProvider router={router}/>
     </div>
   </React.StrictMode>
 );
