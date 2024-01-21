2f26a858142ecd26de631e6dfa43680cd5cf73d6
erik.minasian <erik.minasian@coliquio.de>
2024-01-21 18:09:37 +0400

    feat: shared layout added

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from "./App";
 import PostDetails from "./components/PostDetails";
+import RootLayout from "./Routes/RootLayput";
 import "./main.css";
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
-  {path: '/post-details', element: <PostDetails />}
+  {path: '/', element: <RootLayout />, children: [
+    {path: '/', element: <App />},
+    {path: '/post-details', element: <PostDetails />}
+  ]}
 ])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
